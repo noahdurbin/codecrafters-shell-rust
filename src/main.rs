9e0f34d66d6418f051866e8a84f59563560d4711
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-08-26 21:24:00 -0600

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,38 @@
 #[allow(unused_imports)]
 use std::io::{self, Write};
+use std::collections::HashSet;
 
 fn main() {
-    // Wait for user input
     let stdin = io::stdin();
+    let mut builtins = HashSet::new();
+    builtins.insert("echo");
+    builtins.insert("exit");
+    builtins.insert("type");
     
     loop {
-      print!("$ ");
+        print!("$ ");
+        io::stdout().flush().unwrap();
+        
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        let command = input.trim();
 
-      let mut input = String::new();
-
-      io::stdout().flush().unwrap();
-      
-      stdin.read_line(&mut input).unwrap();
-
-      let command = input.trim();
-
-      if command.is_empty() {
-        continue;
-      } else if command == "exit 0" {
-        break;
-      } else if command.starts_with("echo ") {
-        let echo_content = &command[5..];
-        println!("{}", echo_content);
-      } else {
-        println!("{}: command not found", input.trim());
-      }  
+        if command.is_empty() {
+            continue;
+        } else if command == "exit 0" {
+            break;
+        } else if command.starts_with("echo ") {
+            let echo_content = &command[5..];
+            println!("{}", echo_content);
+        } else if command.starts_with("type ") {
+            let cmd_to_check = &command[5..];
+            if builtins.contains(cmd_to_check) {
+                println!("{} is a shell builtin", cmd_to_check);
+            } else {
+                println!("{}: not found", cmd_to_check);
+            }
+        } else {
+            println!("{}: command not found", command);
+        }  
     }
 }
