bd455d9ad61c7714095a4fc1416c7573cce0b82f
Noah Durbin <13364668+noahdurbin@users.noreply.github.com>
2024-08-27 15:58:29 -0600

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 use std::collections::HashSet;
+use std::env;
+use std::path::Path;
 
 fn main() {
     let stdin = io::stdin();
@@ -13,7 +14,6 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
         let command = input.trim();
-
         if command.is_empty() {
             continue;
         } else if command == "exit 0" {
@@ -26,10 +26,25 @@ fn main() {
             if builtins.contains(cmd_to_check) {
                 println!("{} is a shell builtin", cmd_to_check);
             } else {
-                println!("{}: not found", cmd_to_check);
+                match find_command(cmd_to_check) {
+                    Some(path) => println!("{} is {}", cmd_to_check, path),
+                    None => println!("{}: not found", cmd_to_check),
+                }
             }
         } else {
             println!("{}: command not found", command);
         }  
     }
+}
+
+fn find_command(cmd: &str) -> Option<String> {
+    if let Ok(path) = env::var("PATH") {
+        for dir in path.split(':') {
+            let full_path = Path::new(dir).join(cmd);
+            if full_path.is_file() {
+                return full_path.to_str().map(|s| s.to_string());
+            }
+        }
+    }
+    None
 }
